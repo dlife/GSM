6edd03149b264b0ffa828d60ace3afac8f55ba28
dlife <dieterbenoot@gmail.com>
2013-11-07 08:55:03 +0100

    Versie 0.01
    Eerste test voor deze oefening
    C# Visual Studio

---
+++ GSM/GSM/Enquete.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GSM
+{
+    class Enquete
+    {
+        #region Class variables
+        
+        private int iJa = 0;
+        private int iNee = 0;
+        private int iOnverschillig = 0;
+        private int iCount = 0;
+
+        #endregion
+
+        #region Property's
+
+        public int Ja
+        {
+            get { return iJa; }
+            set { iJa = value; }
+        }
+
+        public int Nee { get { return iNee; } set { iNee = value; } }
+        public int Onverschillig { get { return iOnverschillig; } set { iOnverschillig = value;} }
+        public int Count { get { return iCount; } set { iCount = value; } }
+
+        #endregion
+
+        #region Methods
+
+        public void Reset()
+        {
+            iJa = iNee = iOnverschillig = iCount = 0;
+        }
+
+        public string GeefWeer()
+        {
+            string test, uitvoer;
+
+            if (iJa == iNee)
+            {
+                test = "onbepaald";
+            }
+            else if (iJa > iNee)
+            {
+                test = "toegestaan";
+            }
+            else
+            {
+                test = "niet toegestaan";
+            }
+
+            uitvoer = "GSM-gebruik is "+ test + Environment.NewLine
+                + "-#ja: " + iJa + Environment.NewLine
+                + "-#nee: " + iNee + Environment.NewLine
+                + "-#onverschillig: " + iOnverschillig;
+
+            return uitvoer;
+        }
+
+        public bool IsBereikt()
+        {
+            int reken = iJa + iNee + iOnverschillig;
+            if (reken == iCount)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        #endregion
+    }
+}
---
+++ GSM/GSM/Main.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GSM
+{
+    public partial class Main : Form
+    {
+        private Enquete EN = new Enquete();
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHoeveel_Click(object sender, EventArgs e)
+        {
+            int getal;
+            string invoer = txtHoeveel.Text;
+            if (!Int32.TryParse(invoer,out getal)){reset();return;}
+
+            EN.Count = getal;
+            Switch();
+
+            this.AcceptButton = btnVraag;
+        }
+
+        private void btnEinde_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void reset()
+        {
+            Switch();
+            lblWeergave.Text = "";
+            txtHoeveel.Text = "";
+            txtVraag.Text = "";
+            this.AcceptButton = btnHoeveel;
+            EN.Reset();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        private void btnVraag_Click(object sender, EventArgs e)
+        {
+            string invoer = txtVraag.Text.ToUpper();
+            
+            switch (invoer)
+            {
+                case "J": EN.Ja += 1; 
+                    break;
+                case "N": EN.Nee += 1;
+                    break;
+                case "O": EN.Onverschillig += 1;
+                    break;
+                default:
+                    reset();
+                    return;
+            } 
+                       
+            lblWeergave.Text = EN.GeefWeer(); 
+            txtVraag.Text = "";
+            txtVraag.Focus();
+
+            if (EN.IsBereikt())
+            {
+                btnVraag.Enabled = !btnVraag.Enabled;
+                txtVraag.Enabled = !txtVraag.Enabled;
+                return;
+            }
+        }
+
+        private void Switch()
+        {            
+            txtVraag.Enabled = !txtVraag.Enabled;
+            btnVraag.Enabled = !btnVraag.Enabled;
+            txtHoeveel.Enabled = !txtHoeveel.Enabled;
+            btnHoeveel.Enabled = !btnHoeveel.Enabled;
+        }
+    }
+}
